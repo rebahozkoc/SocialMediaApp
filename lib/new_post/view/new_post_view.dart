fd4c1305af7cde9a602b416a9c13cfb2ca76b9ca
Rebah Özkoç <rebahozkoc@sabanciuniv.edu>
2022-05-30 20:53:03 +0300

    add image firestrore connection

--- lib/new_post/view/new_post_view.dart
+++ lib/new_post/view/new_post_view.dart
@@ -2,9 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:sabanci_talks/new_post/view/add_image_view.dart';
 import 'package:sabanci_talks/new_post/view/added_image_view.dart';
 import 'package:sabanci_talks/util/dimensions.dart';
+import 'package:sabanci_talks/util/screen_sizes.dart';
 import 'new_post_form_view.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:sabanci_talks/util/styles.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'dart:io';
+import 'package:path/path.dart';
 
 class NewPostView extends StatefulWidget {
   const NewPostView({Key? key}) : super(key: key);
@@ -18,15 +22,40 @@ class _NewPostViewState extends State<NewPostView> {
 
   final List<XFile> imgList = [];
 
-  Future pickImage() async {
-    final pickedFile = await _picker.pickImage(source: ImageSource.gallery);
+  Future pickImageCamera() async {
+    final pickedFile = await _picker.pickImage(source: ImageSource.camera);
     setState(() {
-      if (pickedFile == null){
+      if (pickedFile == null) {
         debugPrint("No photo selected");
-      }else{
+      } else {
         imgList.add(pickedFile);
       }
     });
+  }
+
+  Future pickImageGallery() async {
+    final pickedFile = await _picker.pickImage(source: ImageSource.gallery);
+    setState(() {
+      if (pickedFile == null) {
+        debugPrint("No photo selected");
+      } else {
+        imgList.add(pickedFile);
+      }
+    });
+  }
+
+  Future uploadImageToFirebase(BuildContext context, XFile image) async {
+    String fileName = basename(image.path);
+    Reference firebaseStorageRef =
+        FirebaseStorage.instance.ref().child('uploads/$fileName');
+    try {
+      await firebaseStorageRef.putFile(File(image.path));
+      print("Upload complete");
+    } on FirebaseException catch (e) {
+      print('ERROR: ${e.code} - ${e.message}');
+    } catch (e) {
+      print(e.toString());
+    }
   }
 
   @override
@@ -40,11 +69,68 @@ class _NewPostViewState extends State<NewPostView> {
           child: AddedImageView(imgList[i].path)));
     }
 
-    imgViewList.add(
-      InkWell(onTap: pickImage, child: const AddImageView()),
-    );
+    imgViewList.add(InkWell(
+      onTap: () {
+        showGeneralDialog(
+          barrierLabel: "Label",
+          barrierDismissible: true,
+          barrierColor: Colors.black.withOpacity(0.5),
+          transitionDuration: const Duration(milliseconds: 400),
+          context: context,
+          pageBuilder: (context, anim1, anim2) {
+            return Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                child: Wrap(
+                  direction: Axis.vertical,
+                  crossAxisAlignment: WrapCrossAlignment.start,
+                  children: [
+                    SizedBox(
+                        width: screenWidth(context),
+                        child: TextButton(
+                          onPressed: pickImageCamera,
+                          child: Padding(
+                            padding: Dimen.regularParentPadding,
+                            child: Text(
+                              "Take a photo",
+                              style: kButtonDaTextStyle,
+                            ),
+                          ),
+                        )),
+                    SizedBox(
+                        width: screenWidth(context),
+                        child: TextButton(
+                            onPressed: pickImageGallery,
+                            child: Padding(
+                              padding: Dimen.regularParentPadding,
+                              child: Text(
+                                "Select image from galery",
+                                style: kButtonDaTextStyle,
+                              ),
+                            ))),
+                  ],
+                ),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(5),
+                ),
+              ),
+            );
+          },
+          transitionBuilder: (context, anim1, anim2, child) {
+            return SlideTransition(
+              position:
+                  Tween(begin: const Offset(0, 1), end: const Offset(0, 0))
+                      .animate(anim1),
+              child: child,
+            );
+          },
+        );
+      },
+      child: const AddImageView(false),
+    ));
     return Scaffold(
-      appBar: _appBar(),
+      appBar: _appBar(context),
       body: SingleChildScrollView(
         child: Padding(
           padding: Dimen.regularParentPadding,
@@ -54,7 +140,6 @@ class _NewPostViewState extends State<NewPostView> {
               SingleChildScrollView(
                   scrollDirection: Axis.horizontal,
                   child: Row(children: imgViewList)),
-              
             ],
           ),
         ),
@@ -62,7 +147,7 @@ class _NewPostViewState extends State<NewPostView> {
     );
   }
 
-  AppBar _appBar() {
+  AppBar _appBar(BuildContext context) {
     return AppBar(
       title: const Text('New Post'),
       centerTitle: false,
@@ -70,6 +155,7 @@ class _NewPostViewState extends State<NewPostView> {
         TextButton(
           onPressed: () {
             debugPrint("Next");
+            uploadImageToFirebase(context, imgList[0]);
           },
           child: const Text("Share",
               style: TextStyle(
