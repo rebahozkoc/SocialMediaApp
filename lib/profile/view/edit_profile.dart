2a6ca2b4779d96c9a2efd27e8d0e241a64f59b62
gorkem yar <gorkemyar@sabanciuniv.edu>
2022-06-09 23:28:01 +0300

    setProfilePicture

--- lib/profile/view/edit_profile.dart
+++ lib/profile/view/edit_profile.dart
@@ -1,14 +1,20 @@
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:sabanci_talks/firestore_classes/firestore_main/firestore.dart';
 import 'package:sabanci_talks/navigation/navigation_constants.dart';
 import 'package:sabanci_talks/navigation/navigation_service.dart';
 import 'package:sabanci_talks/util/dimensions.dart';
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:sabanci_talks/util/screen_sizes.dart';
 import 'dart:io' show Platform;
 import '../../util/styles.dart';
 import 'package:flutter/cupertino.dart';
+import 'dart:io';
+import 'package:sabanci_talks/util/styles.dart';
+import 'package:path/path.dart';
 
-class EditProfileView extends StatelessWidget {
+class EditProfileView extends StatefulWidget {
   EditProfileView({
     Key? key,
     required this.docId,
@@ -22,13 +28,23 @@ class EditProfileView extends StatelessWidget {
   final String fullNamePre;
   final String biographyPre;
   final String picturePre;
-  final _formKey = GlobalKey<FormState>();
   static const String routeName = '/editProfile';
 
+  @override
+  State<EditProfileView> createState() => _EditProfileViewState();
+}
+
+class _EditProfileViewState extends State<EditProfileView> {
+  final _formKey = GlobalKey<FormState>();
+
   String fullName = '';
+
   String biography = '';
-  String profilePicture = '';
+
+  XFile? myImage;
+
   String gender = '';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -39,10 +55,10 @@ class EditProfileView extends StatelessWidget {
           ),
           actions: [_saveButton(context)],
         ),
-        body: _body());
+        body: _body(context));
   }
 
-  Padding _body() => Padding(
+  Padding _body(BuildContext context) => Padding(
       padding: Dimen.regularParentPadding,
       child: Form(
         key: _formKey,
@@ -55,9 +71,10 @@ class EditProfileView extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.start,
               mainAxisSize: MainAxisSize.max,
               children: [
-                _networkImage(picturePre),
+                _networkImage(
+                    newPicture != null ? newPicture : widget.picturePre),
                 _sized(),
-                _profileImageButton(),
+                _profileImageButton(context),
               ],
             ),
             _divider(),
@@ -80,8 +97,13 @@ class EditProfileView extends StatelessWidget {
       onPressed: () async {
         if (_formKey.currentState!.validate()) {
           _formKey.currentState!.save();
+          if (isButtonActive && myImage != null) {
+            await uploadPostToFirebase(context, myImage!);
+            debugPrint("Uploaded?");
+          }
           Firestore f = Firestore();
-          await f.UpdateUser(docId, fullName, gender, biography, picturePre);
+          await f.UpdateUser(widget.docId, fullName, gender, biography,
+              newPicture != null ? newPicture : widget.picturePre);
           NavigationService.instance
               .navigateToPageClear(path: NavigationConstants.BOTTOM_BAR);
         } else {
@@ -91,9 +113,63 @@ class EditProfileView extends StatelessWidget {
       },
       child: Text("Save", style: kWhiteTextStyle));
 
-  TextButton _profileImageButton() => TextButton(
+  TextButton _profileImageButton(BuildContext context) => TextButton(
         onPressed: () {
-          //_imageModal();
+          showGeneralDialog(
+            barrierLabel: "Label",
+            barrierDismissible: true,
+            barrierColor: Colors.black.withOpacity(0.5),
+            transitionDuration: const Duration(milliseconds: 400),
+            context: context,
+            pageBuilder: (context, anim1, anim2) {
+              return Align(
+                alignment: Alignment.bottomCenter,
+                child: Container(
+                  child: Wrap(
+                    direction: Axis.vertical,
+                    crossAxisAlignment: WrapCrossAlignment.start,
+                    children: [
+                      SizedBox(
+                          width: screenWidth(context),
+                          child: TextButton(
+                            onPressed: pickImageCamera,
+                            child: Padding(
+                              padding: Dimen.regularParentPadding,
+                              child: Text(
+                                "Take a photo",
+                                style: kButtonDaTextStyle,
+                              ),
+                            ),
+                          )),
+                      SizedBox(
+                          width: screenWidth(context),
+                          child: TextButton(
+                              onPressed: pickImageGallery,
+                              child: Padding(
+                                padding: Dimen.regularParentPadding,
+                                child: Text(
+                                  "Select image from galery",
+                                  style: kButtonDaTextStyle,
+                                ),
+                              ))),
+                    ],
+                  ),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                ),
+              );
+            },
+            transitionBuilder: (context, anim1, anim2, child) {
+              return SlideTransition(
+                position:
+                    Tween(begin: const Offset(0, 1), end: const Offset(0, 0))
+                        .animate(anim1),
+                child: child,
+              );
+            },
+          );
         },
         child: const Text("Change Your Profile Photo"),
       );
@@ -146,7 +222,7 @@ class EditProfileView extends StatelessWidget {
         autocorrect: false,
         maxLines: 1,
         style: const TextStyle(fontSize: 14),
-        initialValue: fullNamePre,
+        initialValue: widget.fullNamePre,
         validator: (value) {
           if (value != null) {
             if (value.isEmpty) {
@@ -170,7 +246,7 @@ class EditProfileView extends StatelessWidget {
         textInputAction: TextInputAction.next,
         onChanged: (value) {},
         autocorrect: false,
-        initialValue: genderPre,
+        initialValue: widget.genderPre,
         maxLines: 1,
         style: const TextStyle(fontSize: 14),
         validator: (value) {
@@ -191,7 +267,7 @@ class EditProfileView extends StatelessWidget {
         textInputAction: TextInputAction.done,
         onChanged: (value) {},
         autocorrect: false,
-        initialValue: biographyPre,
+        initialValue: widget.biographyPre,
         maxLines: 1,
         style: const TextStyle(fontSize: 14),
         validator: (value) {
@@ -206,18 +282,29 @@ class EditProfileView extends StatelessWidget {
         },
       );
 
-  CachedNetworkImage _networkImage(profileImage) {
-    return CachedNetworkImage(
-      imageUrl: profileImage,
-      imageBuilder: (context, imageProvider) => Container(
+  Widget _networkImage(profileImage) {
+    debugPrint("newpicture is ${newPicture}");
+
+    if (myImage != null) {
+      return ClipOval(
+          child: Image.file(
+        File(myImage!.path),
         width: 64,
         height: 64,
-        decoration: BoxDecoration(
-          shape: BoxShape.circle,
-          image: DecorationImage(image: imageProvider, fit: BoxFit.cover),
+      ));
+    } else {
+      return CachedNetworkImage(
+        imageUrl: profileImage,
+        imageBuilder: (context, imageProvider) => Container(
+          width: 64,
+          height: 64,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            image: DecorationImage(image: imageProvider, fit: BoxFit.cover),
+          ),
         ),
-      ),
-    );
+      );
+    }
   }
 
   Future<void> _showDialog(
@@ -267,5 +354,67 @@ class EditProfileView extends StatelessWidget {
             );
           }
         });
+  }
+
+  final ImagePicker _picker = ImagePicker();
+
+  Future pickImageCamera() async {
+    final pickedFile = await _picker.pickImage(source: ImageSource.camera);
+    setState(() {
+      if (pickedFile == null) {
+        debugPrint("No photo selected");
+      } else {
+        myImage = pickedFile;
+      }
+    });
+  }
+
+  Future pickImageGallery() async {
+    final pickedFile = await _picker.pickImage(source: ImageSource.gallery);
+    setState(() {
+      if (pickedFile == null) {
+        debugPrint("No photo selected");
+      } else {
+        myImage = pickedFile;
+      }
+    });
+  }
+
+  dynamic newPicture;
+  bool isButtonActive = true;
+  Future uploadPostToFirebase(BuildContext context, XFile image) async {
+    // Check the form for errors
+    //debugPrint("BUTTOON CLICKEEEEEDD");
+    setState(() {
+      isButtonActive = false;
+    });
+
+    //String fileName = basename(image.path);
+
+    // for every image in the list, upload it to firebase
+
+    try {
+      String fileName = basename(image.path);
+      Reference firebaseStorageRef =
+          FirebaseStorage.instance.ref().child('uploads/$fileName');
+
+      await firebaseStorageRef.putFile(File(image.path));
+      debugPrint("image  is ${image.path}");
+      await firebaseStorageRef.getDownloadURL().then((fileURL) {
+        debugPrint("File uploaded");
+        debugPrint(fileURL);
+        setState(() {
+          newPicture = fileURL;
+        });
+      });
+    } on FirebaseException catch (e) {
+      debugPrint('ERROR: ${e.code} - ${e.message}');
+    } catch (e) {
+      debugPrint(e.toString());
+    } finally {
+      setState(() {
+        isButtonActive = true;
+      });
+    }
   }
 }
